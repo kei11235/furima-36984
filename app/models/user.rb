2d90e8d22e128ee105cc5b08d8f86ce6b5aa8530
kumatabetai <90751879+kumatabetai@users.noreply.github.com>
2021-09-28 18:00:34 +0900

    バリデーション(password)修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze, message: 'は6文字以上の半角英数である必要があります'}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: 'は英字と数字の両方を含めて設定してください', allow_blank:true}
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください', allow_blank: true } do
     validates :last_name
