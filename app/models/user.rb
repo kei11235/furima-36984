89f18e7b2fe05add7819158c769a372c3f969a1f
kumatabetai <90751879+kumatabetai@users.noreply.github.com>
2021-09-28 17:55:32 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze, message: 'は6文字以上の半角英数である必要があります'}
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください', allow_blank: true } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角（カタカナ）を使用してください', allow_blank: true } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :date, presence: true
+
 end
