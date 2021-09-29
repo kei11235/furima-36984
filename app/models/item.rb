ecb5687d9ddfff7977ac819bc701339bdf0adb10
kumatabetai <90751879+kumatabetai@users.noreply.github.com>
2021-09-29 18:37:24 +0900

    itemモデルバリデーション生成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,17 @@
 class Item < ApplicationRecord
 
+  validates :name, presence: true
+  validates :explain, presence:true
+  with_options numericality: { other_than: 1 , message: "選択項目が足りません" } do
+    validates :category_id
+    validates :status_id
+    validates :shopping_charge_id
+    validates :area_id
+    validates :days_id
+  end
+  validates :price, presence: true, numericality: { in: 300..9999999, message: 'は￥300~￥9,999,999の範囲で値段をつけてください', allow_blank: true }
+  validates :image, attached_file_presence: true
+
   belongs_to :user
   has_one_attached :image
 
