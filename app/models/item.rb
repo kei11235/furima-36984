b20e78dedba098b2f7ce2805f98c8de78755dde7
kumatabetai <90751879+kumatabetai@users.noreply.github.com>
2021-09-29 18:13:58 +0900

    ActiveHash作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item
+  belongs_to :status
+  belongs_to :shopping_charge
+  belongs_to :area
+  belongs_to :days
+
 end
