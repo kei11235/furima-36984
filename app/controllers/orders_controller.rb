99f46667029ea4ed3bd72a2bd225d34a6d40f90a
kumatabetai <90751879+kumatabetai@users.noreply.github.com>
2021-10-01 19:12:04 +0900

    ordersコントローラー作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,21 @@
 class OrdersController < ApplicationController
 
   def index
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :area_id, :municipalities, :address, :building, :phone_num).merge(user_id: current_user.id, item_id: order.item.id)
   end
 
 end
