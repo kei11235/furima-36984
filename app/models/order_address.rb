5d6b0f7a44212e481059c8eb78858b61727c6c9f
kumatabetai <90751879+kumatabetai@users.noreply.github.com>
2021-10-01 18:34:52 +0900

    OrderAddressクラスのバリデーション

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,15 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :area_id, :municipalities, :address, :building, :phone_num, :order, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は「-（ハイフン）」を用いた半角数字で登録してください", allow_blank: true}
+    validates :municipalities
+    validates :address
+    validates :phone_num, numericality: { with: /0\d{9,10}/, message: 'は半角数字のみ入力してください', allow_blank: true }
+    validates :user_id
+    validates :item_id
+  end
+  validates :area, numericality: { other_than: 1, message: 'は選択項目が足りません' }
+  
 end
