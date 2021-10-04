95e711eaacfd5f250b59cd6dab1906b190539d66
kumatabetai <90751879+kumatabetai@users.noreply.github.com>
2021-10-04 19:35:33 +0900

    building　null:false削除

--- db/migrate/20211001073701_create_addresses.rb
+++ db/migrate/20211001073701_create_addresses.rb
@@ -3,9 +3,9 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
       t.string     :postal_code,    null: false
       t.integer    :area_id,        null: false
-      t.string     :municipalities
+      t.string     :municipalities, null: false
       t.string     :address,        null: false
-      t.string     :building,       null: false
+      t.string     :building
       t.string     :phone_num,      null: false
       t.references :order,          null: false, foreign_key: true
       t.timestamps
